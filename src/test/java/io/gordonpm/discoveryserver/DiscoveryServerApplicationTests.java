3653f0b7be32759a7d64095adfcaa137e3b5b549
gordonpm <gordonm0815@gmail.com>
2022-08-25 12:11:02 -0700

    Initial commit

---
+++ src/test/java/io/gordonpm/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gordonpm.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
